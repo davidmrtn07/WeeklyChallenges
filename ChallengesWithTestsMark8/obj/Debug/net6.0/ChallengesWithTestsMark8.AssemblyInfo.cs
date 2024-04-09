b2c5d93ab0db9744e64bea3d9df84fd47b2ac15a
davidmrtn07 <davidmrtn07@gmail.com>
2024-04-09 16:52:09 -0500

    Weekly Challenges Set 2 Complete

--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1535a7e6558d0bc2d3d84e3d73fd4e4047615b30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ec14c271dc5dbb587a7c4621592d10ad9ee0ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
